b8d15373a712ae72b12bc6047d965cdf42911f71
Shay EK <shayek34@gmail.com>
2020-05-29 10:26:50 +0300

    Shay Client Updated

--- src/main/java/Service/Client.java
+++ src/main/java/Service/Client.java
@@ -13,18 +13,13 @@ public class Client extends Observable {
     String serverName = "localhost";
     int serverPortNumber = 9876;
     Socket socket = null;
-    InputStream is = null;
-    DataInputStream dis = null;
-    OutputStream os = null;
-    DataOutputStream dos = null;
 
     public String openConnection(String data){
-        String ans="";
         String serverResponse="";
         try {
             socket = new Socket(serverName, serverPortNumber);
             BufferedReader input = new BufferedReader(new InputStreamReader((socket.getInputStream())));
-//            BufferedReader output = new BufferedReader(new OutputStr((socket.getOutputStream())));
+            //BufferedReader output = new BufferedReader(new OutputStr((socket.getOutputStream())));
             BufferedReader keyboard= new BufferedReader((new InputStreamReader((System.in))));
             PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
             out.println(data);
